3eda497ab46702e56275e7aec5e5212b9f22c149
eduardo divardin <luisedivardin@live.com>
2021-03-04 22:29:46 -0300

    Adicionar arquivos de projeto.

---
+++ Nome e idade com cor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processar();
+
+            Console.ReadKey();
+        }
+
+        private static void Processar()
+        {
+            int Idade;
+            Console.Write("Informe a sua idade: ");
+            int.TryParse(Console.ReadLine(), out Idade);
+
+            if (Idade >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Acesso permitdo");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Acesso Negado");
+            }
+        }
+    }
+}
